c32d282287308dcbfeb54cce1a954bcbe822fa99
Kyler Kubo <kubok@rpi.edu>
2019-02-06 16:56:51 -0500

    started on pause menu

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    bool paused = false;
+    public Transform Canvas;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+            if (Canvas.gameObject.activeInHierarchy == false)
+            {
+                Canvas.gameObject.SetActive(true);
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Canvas.gameObject.SetActive(false);
+                Time.timeScale = 1f;
+            }
+    }
+}
