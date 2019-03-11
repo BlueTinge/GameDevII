eac87c5d18f53b30dd143d78f5a30f25bb98087b
Max Sichel <msik314@gmail.com>
2019-03-11 03:34:25 -0400

    Add eye movement

--- Assets/Scripts/Enemies/EyeEnemy.cs
+++ Assets/Scripts/Enemies/EyeEnemy.cs
@@ -8,7 +8,12 @@ using Stargaze.AI;
 public class EyeEnemy : MonoBehaviour, IEnemy
 {
     [SerializeField]private float moveTime;
-    [SerializeField]private float moveSpeed;
+    [SerializeField]private float maxAccel;
+    [SerializeField]private float maxSpeed;
+    [SerializeField]private float slowRadius;
+    [SerializeField]private float targetRadius;
+    [SerializeField]private float accelTime;
+    [SerializeField]private float goalDistance;
     [SerializeField]private float windupTime;
     [SerializeField]private float coolDown;
     private BehaviorTree behaviorTree;
@@ -16,20 +21,38 @@ public class EyeEnemy : MonoBehaviour, IEnemy
     private HealthStats healthStats;
     private Rigidbody rb;
     private Transform target;
+    private Vector3 accel;
+    private bool canMove;
 
     void Awake()
     {
         healthStats = GetComponent<HealthStats>();
         laserCharge = GetComponentInChildren<EyeCharge>();
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        accel = Vector3.zero;
+        canMove = true;
         behaviorTree = new BehaviorTree
         (
             new SequenceTask(new ITreeTask[]
             {
+                new WhileTask
+                (
+                    new NotTask
+                    (
+                        new DelayTask(moveTime)
+                    ),
+                    new BasicMove(this, 0)
+                ),
+                new CallTask(() => {EndMove(); return true;}),
                 new BasicAttack(this, windupTime),
                 new DelayTask(coolDown)
             })
         );
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -43,7 +66,8 @@ public class EyeEnemy : MonoBehaviour, IEnemy
 
     void FixedUpdate()
     {
-
+        if(!canMove) Stop();
+        rb.AddForce(accel, ForceMode.Acceleration);
     }
 
     public bool Target()
@@ -52,7 +76,54 @@ public class EyeEnemy : MonoBehaviour, IEnemy
     }
     public void Move()
     {
+        canMove = true;
+        Vector3 dir = transform.position - target.position;
+        dir.y = 0;
+        dir = dir.normalized * goalDistance;
+        Vector3 t = dir + target.position;
+        t.y = transform.position.y;
 
+        Vector3 v = t - transform.position;
+        float dist = v.magnitude;
+        if(dist < targetRadius)
+        {
+            accel = Vector3.zero;
+            return;
+        }
+
+        float speed = maxSpeed;
+        if(dist < slowRadius)
+        {
+            speed = maxSpeed * dist / slowRadius;
+        }
+
+        Vector3 a = v - rb.velocity;
+        a /= accelTime;
+        if(a.sqrMagnitude > maxAccel * maxAccel)
+        {
+            accel = a.normalized * maxAccel;
+        }
+        else
+        {
+            accel = a;
+        }
+    }
+
+    private void Stop()
+    {
+        if(rb.velocity.sqrMagnitude <= accelTime * accelTime * maxAccel * maxAccel)
+        {
+            accel = Vector3.zero;
+            rb.velocity = Vector3.zero;
+        }
+        else
+        {
+            accel = -maxAccel * rb.velocity.normalized;
+        }
+    }
+    public void EndMove()
+    {
+        canMove = false;
     }
 
     public void Attack()
