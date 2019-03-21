72ffb1743340ab251061a638be3a3c32736c2c29
Jay <totallyawesomecraft@gmail.com>
2019-03-20 22:00:46 -0400

    Gates are also 100% functional
    Still need sound though
    Also you can dash through them but thats more of a problem with dashing than with gates

---
+++ Assets/Scripts/LevelElements/Gate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Gate : Activatable
+{
+    public Animator Animator;
+
+    void Awake()
+    {
+        Animator = GetComponent<Animator>();
+    }
+
+    public override void Activate()
+    {
+        Animator.SetBool("Raised", true);
+    }
+
+    public override void Deactivate()
+    {
+        Animator.SetBool("Raised", false);
+    }
+
+    public override bool GetIsActivated()
+    {
+        return Animator.GetBool("Raised");
+    }
+}
